70e9425b76ad6a19283d1cfd8e93287bb546faf1
KonTroAl <troshenkin.k@yandex.ru>
2022-01-29 12:46:04 +0500

    create telbot

---
+++ telbot.py
@@ -0,0 +1,24 @@
+from telebot import TeleBot
+
+from settings import settings
+
+from handlers.handler_main import HandlerMain
+
+class TelBot:
+
+    __version__ = settings.VERSION
+    __author__ = settings.AUTHOR
+
+    def __init__(self):
+        self.token = settings.TOKEN
+        self.bot = TeleBot(self.token)
+
+        self.handler = HandlerMain(self.bot)
+
+    def start(self):
+        self.handler.handle()
+
+    def run_bot(self):
+        self.start()
+
+        self.bot.polling(none_stop=True)
